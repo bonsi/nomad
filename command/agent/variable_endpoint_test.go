103b60aacea452450b2aa345d5ae73f2fa7965be
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2022-05-11 03:28:43 -0400

    Tests; can't run parallel because of global state.

--- command/agent/variable_endpoint_test.go
+++ command/agent/variable_endpoint_test.go
@@ -1,23 +1,38 @@
 package agent
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/hashicorp/nomad/ci"
 	"github.com/hashicorp/nomad/nomad/mock"
 	"github.com/hashicorp/nomad/nomad/structs"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHTTP_SecureVariableList(t *testing.T) {
-	ci.Parallel(t)
+	//ci.Parallel(t)
 	httpTest(t, nil, func(s *TestAgent) {
+		// Test the empty list case
+		req, err := http.NewRequest("GET", "/v1/vars", nil)
+		require.NoError(t, err)
+		respW := httptest.NewRecorder()
+
+		// Make the request
+		obj, err := s.Server.SecureVariablesRequest(respW, req)
+		require.NoError(t, err)
+
+		// add vars and test a populated backend
 		sv1 := mock.SecureVariable()
 		sv2 := mock.SecureVariable()
 		sv3 := mock.SecureVariable()
-		for _, sv := range []*structs.SecureVariable{sv1, sv2, sv3} {
+		sv4 := mock.SecureVariable()
+		sv4.Path = sv1.Path + "/child"
+		for _, sv := range []*structs.SecureVariable{sv1, sv2, sv3, sv4} {
 			args := structs.SecureVariableUpsertRequest{
 				SecureVariable: sv,
 				WriteRequest:   structs.WriteRequest{Region: "global"},
@@ -27,12 +42,12 @@ func TestHTTP_SecureVariableList(t *testing.T) {
 			// require.Nil(s.Agent.RPC("SecureVariable.UpsertSecureVariables", &args, &resp))
 		}
 		// Make the HTTP request
-		req, err := http.NewRequest("GET", "/v1/vars", nil)
+		req, err = http.NewRequest("GET", "/v1/vars", nil)
 		require.NoError(t, err)
-		respW := httptest.NewRecorder()
+		respW = httptest.NewRecorder()
 
 		// Make the request
-		obj, err := s.Server.SecureVariablesRequest(respW, req)
+		obj, err = s.Server.SecureVariablesRequest(respW, req)
 		require.NoError(t, err)
 
 		// Check for the index
@@ -41,13 +56,46 @@ func TestHTTP_SecureVariableList(t *testing.T) {
 		require.NotZero(t, respW.HeaderMap.Get("X-Nomad-LastContact"))
 
 		// Check the output (the 3 we register )
-		require.Len(t, obj.([]*structs.SecureVariable), 3)
+		require.Len(t, obj.([]*structs.SecureVariableStub), 4)
+
+		// test prefix query
+		req, err = http.NewRequest("GET", "/v1/vars?prefix="+sv1.Path, nil)
+		require.NoError(t, err)
+		respW = httptest.NewRecorder()
+
+		// Make the request
+		obj, err = s.Server.SecureVariablesRequest(respW, req)
+		require.NoError(t, err)
+		require.Len(t, obj.([]*structs.SecureVariableStub), 2)
+
 	})
 }
 
 func TestHTTP_SecureVariableQuery(t *testing.T) {
-	ci.Parallel(t)
+	//ci.Parallel(t)
 	httpTest(t, nil, func(s *TestAgent) {
+		// Make a request for a non-existent variable
+		req, err := http.NewRequest("GET", "/v1/var/does/not/exist", nil)
+		require.NoError(t, err)
+		respW := httptest.NewRecorder()
+		obj, err := s.Server.SecureVariableSpecificRequest(respW, req)
+		require.EqualError(t, err, "Secure variable not found")
+
+		// Don't pass a path
+		req, err = http.NewRequest("GET", "/v1/var/", nil)
+		require.NoError(t, err)
+		respW = httptest.NewRecorder()
+		obj, err = s.Server.SecureVariableSpecificRequest(respW, req)
+		require.EqualError(t, err, "Missing secure variable path")
+
+		// Use an incorrect verb
+		req, err = http.NewRequest("LOLWUT", "/v1/var/foo", nil)
+		require.NoError(t, err)
+		respW = httptest.NewRecorder()
+		obj, err = s.Server.SecureVariableSpecificRequest(respW, req)
+		require.EqualError(t, err, ErrInvalidMethod)
+
+		// Query a variable
 		sv1 := mock.SecureVariable()
 		args := structs.SecureVariableUpsertRequest{
 			SecureVariable: sv1,
@@ -58,12 +106,12 @@ func TestHTTP_SecureVariableQuery(t *testing.T) {
 		//require.Nil(s.Agent.RPC("SecureVariable.UpsertSecureVariables", &args, &resp))
 
 		// Make the HTTP request
-		req, err := http.NewRequest("GET", "/v1/var/"+sv1.Path, nil)
+		req, err = http.NewRequest("GET", "/v1/var/"+sv1.Path, nil)
 		require.NoError(t, err)
-		respW := httptest.NewRecorder()
+		respW = httptest.NewRecorder()
 
 		// Make the request
-		obj, err := s.Server.SecureVariableSpecificRequest(respW, req)
+		obj, err = s.Server.SecureVariableSpecificRequest(respW, req)
 		require.NoError(t, err)
 
 		// Check for the index
@@ -77,11 +125,22 @@ func TestHTTP_SecureVariableQuery(t *testing.T) {
 }
 
 func TestHTTP_SecureVariableCreate(t *testing.T) {
-	ci.Parallel(t)
+	//ci.Parallel(t)
 	httpTest(t, nil, func(s *TestAgent) {
+		sv1 := mock.SecureVariable()
+		args := structs.SecureVariableUpsertRequest{
+			SecureVariable: sv1,
+			WriteRequest:   structs.WriteRequest{Region: "global"},
+		}
+		var resp structs.GenericResponse
+		SV_Upsert(&args, &resp)
+
+		// Make a change for update
+		sv1U := sv1.Copy()
+		sv1U.Items["newness"] = "awwyeah"
+
 		// Make the HTTP request
-		sv1 := *mock.SecureVariable()
-		buf := encodeReq(sv1)
+		buf := encodeReq(&sv1U)
 		req, err := http.NewRequest("PUT", "/v1/var/"+sv1.Path, buf)
 		require.NoError(t, err)
 		respW := httptest.NewRecorder()
@@ -100,12 +159,27 @@ func TestHTTP_SecureVariableCreate(t *testing.T) {
 
 		sv1.CreateIndex, sv1.ModifyIndex = out.CreateIndex, out.ModifyIndex
 		require.Equal(t, sv1.Path, out.Path)
-		require.Equal(t, sv1, out)
+		require.NotEqual(t, sv1, out)
+		require.Contains(t, out.Items, "newness")
+
+		// break the request body
+		badBuf := encodeBrokenReq(&sv1U)
+
+		req, err = http.NewRequest("PUT", "/v1/var/"+sv1.Path, badBuf)
+		require.NoError(t, err)
+		respW = httptest.NewRecorder()
+
+		// Make the request
+		obj, err = s.Server.SecureVariableSpecificRequest(respW, req)
+		require.EqualError(t, err, "unexpected EOF")
+		var cErr HTTPCodedError
+		require.ErrorAs(t, err, &cErr)
+		require.Equal(t, http.StatusBadRequest, cErr.Code())
 	})
 }
 
 func TestHTTP_SecureVariableUpdate(t *testing.T) {
-	ci.Parallel(t)
+	//ci.Parallel(t)
 	httpTest(t, nil, func(s *TestAgent) {
 		// Make the HTTP request
 		sv1 := *mock.SecureVariable()
@@ -133,7 +207,7 @@ func TestHTTP_SecureVariableUpdate(t *testing.T) {
 }
 
 func TestHTTP_SecureVariableDelete(t *testing.T) {
-	ci.Parallel(t)
+	//ci.Parallel(t)
 	defer mvs.Reset()
 	httpTest(t, nil, func(s *TestAgent) {
 		sv1 := mock.SecureVariable()
@@ -162,4 +236,13 @@ func TestHTTP_SecureVariableDelete(t *testing.T) {
 		out := mvs.Get(sv1.Path)
 		require.Nil(t, out)
 	})
+}
+
+func encodeBrokenReq(obj interface{}) io.ReadCloser {
+	// var buf *bytes.Buffer
+	// enc := json.NewEncoder(buf)
+	// enc.Encode(obj)
+	b, _ := json.Marshal(obj)
+	b = b[0 : len(b)-5] // strip newline and final }
+	return ioutil.NopCloser(bytes.NewReader(b))
 }
--- command/agent/variable_mock_test.go
+++ command/agent/variable_mock_test.go
@@ -8,19 +8,21 @@ import (
 )
 
 func TestMockVariables(t *testing.T) {
+	defer mvs.Reset()
 	sv1 := mock.SecureVariable()
 	mvs.Add(sv1.Path, *sv1)
-	out := *mvs.List()
+	out := mvs.List("")
 	require.NotNil(t, out)
 	require.Len(t, out, 1)
 }
 
 func TestDeleteMockVariables(t *testing.T) {
+	defer mvs.Reset()
 	sv1 := mock.SecureVariable()
 	mvs.Add(sv1.Path, *sv1)
-	out := *mvs.List()
+	out := mvs.List("")
 	require.NotNil(t, out)
 	require.Len(t, out, 1)
 	mvs.Delete(sv1.Path)
-	require.Nil(t, mvs.List())
+	require.Empty(t, mvs.List(""))
 }
